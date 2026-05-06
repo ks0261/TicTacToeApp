71d6a7a94b3b534379fcd39af36c99efec8ac9fb
ks0261 <ks0261@srmist.edu.in>
2026-05-06 14:34:48 +0530

    FeatureUC9

--- TicTacToeApp/src/main/java/TicTacToeApp.java
+++ TicTacToeApp/src/main/java/TicTacToeApp.java
@@ -1,88 +1,51 @@
 public class TicTacToeApp{
 
-    static char[][] board = {
-            {'-', '-', '-'},
-            {'-', '-', '-'},
-            {'-', '-', '-'}
-    };
-
-    static boolean isHumanTurn = true;
-    static boolean gameOver = false;
+    static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
 
-        while (!gameOver) {
+        board = new char[][] {
+                {'X', 'O', 'X'},
+                {'O', 'X', 'O'},
+                {'O', 'X', 'X'}
+        };
 
-            if (isHumanTurn) {
-                humanMove();
-            } else {
-                computerMove();
-            }
-
-            printBoard();
-
-            if (hasWon('X') || hasWon('O') || isBoardFull()) {
-                gameOver = true;
-            } else {
-                isHumanTurn = !isHumanTurn;
-            }
-        }
+        System.out.println(hasWon('X')); // true
     }
 
-    static void humanMove() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == '-') {
-                    board[i][j] = 'X';
-                    return;
-                }
-            }
-        }
-    }
-
-    static void computerMove() {
-        for (int i = 2; i >= 0; i--) {
-            for (int j = 2; j >= 0; j--) {
-                if (board[i][j] == '-') {
-                    board[i][j] = 'O';
-                    return;
-                }
-            }
-        }
-    }
 
     static boolean hasWon(char symbol) {
 
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) return true;
+            if (board[i][0] == symbol &&
+                    board[i][1] == symbol &&
+                    board[i][2] == symbol) {
+                return true;
+            }
         }
 
         for (int j = 0; j < 3; j++) {
-            if (board[0][j] == symbol && board[1][j] == symbol && board[2][j] == symbol) return true;
+            if (board[0][j] == symbol &&
+                    board[1][j] == symbol &&
+                    board[2][j] == symbol) {
+                return true;
+            }
         }
 
-        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) return true;
-        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) return true;
+
+        if (board[0][0] == symbol &&
+                board[1][1] == symbol &&
+                board[2][2] == symbol) {
+            return true;
+        }
+
+
+        if (board[0][2] == symbol &&
+                board[1][1] == symbol &&
+                board[2][0] == symbol) {
+            return true;
+        }
 
         return false;
-    }
-
-    static boolean isBoardFull() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == '-') return false;
-            }
-        }
-        return true;
-    }
-
-    static void printBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
     }
 }
